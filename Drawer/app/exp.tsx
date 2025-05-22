9fc2d8c5130b5ed8f7d915cca59c806e72c7c284
Bharathhh30 <vnsb30@gmail.com>
2025-05-22 23:51:43 +0530

    n\

---
+++ Drawer/app/exp.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const exp = () => {
+  return (
+    <View>
+      <Text>Tryinh to experminet</Text>
+    </View>
+  )
+}
+
+export default exp
